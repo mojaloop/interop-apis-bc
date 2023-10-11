50293b101fc7d64168e78f2014541249275c014a
Rui Rocha <rui.rocha.work@outlook.com>
2023-10-11 01:27:28 +0100

    [ci skip] lint fix

--- packages/fspiop-utils-lib/src/transformer.ts
+++ packages/fspiop-utils-lib/src/transformer.ts
@@ -702,7 +702,7 @@ export const transformPayloadBulkTransferRequestPost = (payload: BulkTransferPre
 					amount: individualTransfer.amount,
 					currency: individualTransfer.currencyCode
 				}
-			}
+			};
 		}),
 		extensionList: payload.extensionList
 	};
